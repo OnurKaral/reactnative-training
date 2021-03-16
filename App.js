6c1b6d2abbf6d79698f2e39a8c6d5e19777583d3
Onur Karal <santobixua@gmail.com>
2021-03-16 14:42:32 +0300

    import appjs

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import ComponentsScreen from './src/screens/ComponentsScreen';
+import ComponentsScreen from './src/screens/ListScreen';
 
 const App = () => {
   return (
     <View>
       <ComponentsScreen />
+      <ListScreen />
     </View>
   );
 };

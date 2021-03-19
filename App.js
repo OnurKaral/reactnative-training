f6bc36feff9bfc225cf623773c172e43d0c0d218
Onur Karal <santobixua@gmail.com>
2021-03-19 18:22:50 +0300

    RN Navigation

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import {StyleSheet, Text, View} from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
+import ComponentsScreen from './src/screens/ComponentsScreen';
 import ListScreen from './src/screens/ListScreen';
 
-const App = () => {
-  return <ListScreen />;
-};
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Overview'}}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-function HomeScreen() {
+function DetailScreen() {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
+      <Text>Details Screen</Text>
     </View>
   );
 }
 
-export default HomeScreen;
+export default DetailScreen;

caa81fc392280730b071a03bc6089406f32705a5
Onur Karal <santobixua@gmail.com>
2021-03-29 22:13:46 +0300

    day4-auth

---
+++ day4-auth/src/screens/GetScreen.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import axios from 'axios';
+
+import {Button, View, Text, StyleSheet, Image} from 'react-native';
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
+});
+
+function GetScreen({navigation}) {
+  const url = '';
+  const axios = require('axios').default;
+
+  axios
+    .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then(function (response) {
+      // handle success
+      url = response.data.hdurl;
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text></Text>
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: url,
+        }}
+      />
+    </View>
+  );
+}
+
+export default GetScreen;
